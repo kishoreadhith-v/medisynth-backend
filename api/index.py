3997f56a4f327ba890e61bd7a8750cf64b229511
kishoreadhith-v <kishoreadhithvijayakumaar@gmail.com>
2024-06-18 18:31:55 +0530

    chore: Refactor Flask app initialization and import statements

--- api/index.py
+++ api/index.py
@@ -17,13 +17,12 @@ client = MongoClient(os.getenv('MONGO_URI'))
 db = client['dev-db']
 
 # Import blueprints
-from .routes.patient_routes import patient_bp
-from .routes.anomaly_routes import anomaly_bp
-from .routes.care_plan_routes import care_plan_bp
-from .routes.diagnose_routes import diagnose_bp
+from routes.patient_routes import patient_bp
+from routes.anomaly_routes import anomaly_bp
+from routes.care_plan_routes import care_plan_bp
+from routes.diagnose_routes import diagnose_bp
 
 app = Flask(__name__)
-CORS(app)
 
 app.register_blueprint(patient_bp)
 app.register_blueprint(anomaly_bp)
